af09395d23712f77466cf092a0040702ccc6bd90
Cihan Sakarya <cihansakaryacs@gmail.com>
2021-10-09 23:12:57 +0300

    feat: Update test code

--- Source/Sandbox/Private/EntryPoint.cpp
+++ Source/Sandbox/Private/EntryPoint.cpp
@@ -1,5 +1,25 @@
+#include "Macros.h"
+#include "ConfigClient.h"
+
+
+#include <iostream>
+
+#include <windows.h>
+
 int main()
 {
+	ConfigClient cc;
+	cc.ConnectToServer("localhost", "5555");
 	
+	while (1)
+	{
+		static char x[2];
+		x[0]++;
+		cc.WriteBytes(x, 2);
+
+		Sleep(200);
+	}
+
+
 	return 0;
 }
